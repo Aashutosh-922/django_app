e44d64e737bcfc52e80722ec95bdb2e705c40901
Aashutosh-922 <karaleash462@gmail.com>
2023-06-03 00:56:30 +0530

    admin panel update

--- todo_list/todo_app/urls.py
+++ todo_list/todo_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 from todo_app.views import TodoItemViewSet
+from django.contrib import admin
+from django.urls import path
 
 router = routers.DefaultRouter()
 router.register(r'todoitems', TodoItemViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
